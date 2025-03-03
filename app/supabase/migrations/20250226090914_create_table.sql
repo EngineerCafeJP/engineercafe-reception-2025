80441f59e57b4d41d8f2432f00b4a059f0333f89
EngineerCafe A <>
2025-03-03 17:12:46 +0900

    add seat_usage_logs

--- app/supabase/migrations/20250226090914_create_table.sql
+++ app/supabase/migrations/20250226090914_create_table.sql
@@ -1,11 +1,14 @@
 -- 都道府県テーブル
-CREATE TABLE IF NOT EXISTS prefectures (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name text NOT NULL,
-  created_at timestamptz DEFAULT now()
-);
+CREATE TABLE
+  IF NOT EXISTS prefectures (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL,
+    created_at timestamptz DEFAULT now ()
+  );
 
-INSERT INTO prefectures (name) VALUES
+INSERT INTO
+  prefectures (name)
+VALUES
   ('北海道'),
   ('青森県'),
   ('岩手県'),
@@ -55,26 +58,32 @@ INSERT INTO prefectures (name) VALUES
   ('沖縄県');
 
 -- 所属テーブル
-CREATE TABLE IF NOT EXISTS belongs (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name text NOT NULL,
-  created_at timestamptz DEFAULT now()
-);
+CREATE TABLE
+  IF NOT EXISTS belongs (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL,
+    created_at timestamptz DEFAULT now ()
+  );
 
-INSERT INTO belongs (name) VALUES
+INSERT INTO
+  belongs (name)
+VALUES
   ('個人'),
   ('コミュニティー'),
   ('法人'),
   ('その他');
 
 -- 仕事テーブル
-CREATE TABLE IF NOT EXISTS jobs (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name text NOT NULL,
-  created_at timestamptz DEFAULT now()
-);
+CREATE TABLE
+  IF NOT EXISTS jobs (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL,
+    created_at timestamptz DEFAULT now ()
+  );
 
-INSERT INTO jobs (name) VALUES
+INSERT INTO
+  jobs (name)
+VALUES
   ('学生'),
   ('ネットワークエンジニア'),
   ('組み込みエンジニア'),
@@ -91,13 +100,16 @@ INSERT INTO jobs (name) VALUES
   ('その他');
 
 -- きっかけテーブル
-CREATE TABLE IF NOT EXISTS founds (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name text NOT NULL,
-  created_at timestamptz DEFAULT now()
-);
+CREATE TABLE
+  IF NOT EXISTS founds (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL,
+    created_at timestamptz DEFAULT now ()
+  );
 
-INSERT INTO founds (name) VALUES
+INSERT INTO
+  founds (name)
+VALUES
   ('SNS'),
   ('職場'),
   ('イベント'),
@@ -108,14 +120,17 @@ INSERT INTO founds (name) VALUES
   ('その他');
 
 -- 座席カテゴリテーブル
-CREATE TABLE IF NOT EXISTS seat_categories (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name text NOT NULL,
-  description text,
-  created_at timestamptz DEFAULT now()
-);
+CREATE TABLE
+  IF NOT EXISTS seat_categories (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL,
+    description text,
+    created_at timestamptz DEFAULT now ()
+  );
 
-INSERT INTO seat_categories (name) VALUES
+INSERT INTO
+  seat_categories (name)
+VALUES
   ('メイン'),
   ('集中'),
   ('アンダー'),
@@ -124,102 +139,273 @@ INSERT INTO seat_categories (name) VALUES
   ('テラス');
 
 -- 座席テーブル
-CREATE TABLE IF NOT EXISTS seats (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  category_id bigint NOT NULL,
-  name text NOT NULL,
-  created_at timestamptz DEFAULT now(),
-  FOREIGN KEY (category_id) REFERENCES seat_categories(id)
-);
+CREATE TABLE
+  IF NOT EXISTS seats (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    category_id bigint NOT NULL,
+    name text NOT NULL,
+    created_at timestamptz DEFAULT now (),
+    FOREIGN KEY (category_id) REFERENCES seat_categories (id)
+  );
 
-INSERT INTO seats (category_id, name) VALUES
+INSERT INTO
+  seats (category_id, name)
+VALUES
   -- メインカテゴリの座席
-  ((SELECT id FROM seat_categories WHERE name = 'メイン'), 'メイン-1'),
-  ((SELECT id FROM seat_categories WHERE name = 'メイン'), 'メイン-2'),
-
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'メイン'
+    ),
+    'メイン-1'
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'メイン'
+    ),
+    'メイン-2'
+  ),
   -- 集中カテゴリの座席
-  ((SELECT id FROM seat_categories WHERE name = '集中'), '集中-1'),
-  ((SELECT id FROM seat_categories WHERE name = '集中'), '集中-2'),
-
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = '集中'
+    ),
+    '集中-1'
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = '集中'
+    ),
+    '集中-2'
+  ),
   -- アンダーカテゴリの座席
-  ((SELECT id FROM seat_categories WHERE name = 'アンダー'), 'アンダー-1'),
-  ((SELECT id FROM seat_categories WHERE name = 'アンダー'), 'アンダー-2'),
-
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'アンダー'
+    ),
+    'アンダー-1'
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'アンダー'
+    ),
+    'アンダー-2'
+  ),
   -- メーカーズカテゴリの座席
-  ((SELECT id FROM seat_categories WHERE name = 'メーカーズ'), 'メーカーズ-1'),
-  ((SELECT id FROM seat_categories WHERE name = 'メーカーズ'), 'メーカーズ-2'),
-
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'メーカーズ'
+    ),
+    'メーカーズ-1'
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'メーカーズ'
+    ),
+    'メーカーズ-2'
+  ),
   -- ミーティングカテゴリの座席
-  ((SELECT id FROM seat_categories WHERE name = 'ミーティング'), 'ミーティング-1'),
-  ((SELECT id FROM seat_categories WHERE name = 'ミーティング'), 'ミーティング-2'),
-
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'ミーティング'
+    ),
+    'ミーティング-1'
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'ミーティング'
+    ),
+    'ミーティング-2'
+  ),
   -- テラスカテゴリの座席
-  ((SELECT id FROM seat_categories WHERE name = 'テラス'), 'テラス-1'),
-  ((SELECT id FROM seat_categories WHERE name = 'テラス'), 'テラス-2');
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'テラス'
+    ),
+    'テラス-1'
+  ),
+  (
+    (
+      SELECT
+        id
+      FROM
+        seat_categories
+      WHERE
+        name = 'テラス'
+    ),
+    'テラス-2'
+  );
 
-CREATE TABLE IF NOT EXISTS users (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  number bigint UNIQUE,
-  name text,
-  pronunciation text,
-  email text,
-  phone text,
-  address1 text,
-  address2 text,
-  city text,
-  prefecture_id bigint,       -- prefecturesテーブルのIDを参照
-  belongs_id bigint,           -- belongsテーブルのIDを参照
-  belongs_other text,          -- 所属が「その他」の場合の自由入力欄
-  belongs_detail text,
-  job_id bigint,               -- jobsテーブルのIDを参照
-  job_other text,              -- 職業が「その他」の場合の自由入力欄
-  found_id bigint,             -- foundsテーブルのIDを参照
-  found_other text,            -- 見つけたきっかけが「その他」の場合の自由入力欄
-  comments text,
-  warnings text,
-  created_at timestamptz DEFAULT now(),
-  FOREIGN KEY (prefecture_id) REFERENCES prefectures(id),
-  FOREIGN KEY (belongs_id) REFERENCES belongs(id),
-  FOREIGN KEY (job_id) REFERENCES jobs(id),
-  FOREIGN KEY (found_id) REFERENCES founds(id)
-);
+CREATE TABLE
+  IF NOT EXISTS users (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    number bigint UNIQUE,
+    name text,
+    pronunciation text,
+    email text,
+    phone text,
+    address1 text,
+    address2 text,
+    city text,
+    prefecture_id bigint,
+    -- prefecturesテーブルのIDを参照
+    belongs_id bigint,
+    -- belongsテーブルのIDを参照
+    belongs_other text,
+    -- 所属が「その他」の場合の自由入力欄
+    belongs_detail text,
+    job_id bigint,
+    -- jobsテーブルのIDを参照
+    job_other text,
+    -- 職業が「その他」の場合の自由入力欄
+    found_id bigint,
+    -- foundsテーブルのIDを参照
+    found_other text,
+    -- 見つけたきっかけが「その他」の場合の自由入力欄
+    comments text,
+    warnings text,
+    created_at timestamptz DEFAULT now (),
+    FOREIGN KEY (prefecture_id) REFERENCES prefectures (id),
+    FOREIGN KEY (belongs_id) REFERENCES belongs (id),
+    FOREIGN KEY (job_id) REFERENCES jobs (id),
+    FOREIGN KEY (found_id) REFERENCES founds (id)
+  );
 
-INSERT INTO users (
-  number,
-  name,
-  pronunciation,
-  email,
-  phone,
-  address1,
-  address2,
-  city,
-  prefecture_id,
-  belongs_id,
-  belongs_detail,
-  job_id,
-  found_id,
-  comments
-) VALUES (
-  '000123',
-  '山田太郎',
-  'やまだたろう',
-  'taro@example.com',
-  '09012345678',
-  '神南1',
-  '',
-  '渋谷区',
-  (SELECT id FROM prefectures WHERE name = '東京都'),
-  (SELECT id FROM belongs WHERE name = '個人'),
-  (SELECT id FROM jobs WHERE name = '学生'),
-  'サンプルの詳細情報です。'
-  (SELECT id FROM founds WHERE name = 'SNS'),
-  'サンプルのコメントです。',
-);
+INSERT INTO
+  users (
+    number,
+    name,
+    pronunciation,
+    email,
+    phone,
+    address1,
+    address2,
+    city,
+    prefecture_id,
+    belongs_id,
+    belongs_detail,
+    job_id,
+    found_id,
+    comments
+  )
+VALUES
+  (
+    '000123',
+    '山田太郎',
+    'やまだたろう',
+    'taro@example.com',
+    '09012345678',
+    '神南1',
+    '',
+    '渋谷区',
+    (
+      SELECT
+        id
+      FROM
+        prefectures
+      WHERE
+        name = '東京都'
+    ),
+    (
+      SELECT
+        id
+      FROM
+        belongs
+      WHERE
+        name = '個人'
+    ),
+    (
+      SELECT
+        id
+      FROM
+        jobs
+      WHERE
+        name = '学生'
+    ),
+    'サンプルの詳細情報です。' (
+      SELECT
+        id
+      FROM
+        founds
+      WHERE
+        name = 'SNS'
+    ),
+    'サンプルのコメントです。',
+  );
 
 -- NFC
-CREATE TABLE IF NOT EXISTS nfcs (
-  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  user_id bigint,
-  nfc_id TEXT UNIQUE,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-);
+CREATE TABLE
+  IF NOT EXISTS nfcs (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id bigint,
+    nfc_id TEXT UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+  );
+
+-- 座席利用
+CREATE TABLE
+  IF NOT EXISTS seat_usage_logs (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    seat_id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    start_time timestamptz NOT NULL,
+    end_time timestamptz,
+    remarks text,
+    created_at timestamptz NOT NULL DEFAULT now (),
+    FOREIGN KEY (seat_id) REFERENCES seats (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+  );
