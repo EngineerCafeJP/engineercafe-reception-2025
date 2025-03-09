9945cf88275abd1cbea34de2d1efc66574b68093
kykt35 <kykt35@gmail.com>
2025-03-09 14:32:44 +0900

    Update sign-in page and header adding sign in action adding logout button to Header

--- app/app/sign-in/page.tsx
+++ app/app/sign-in/page.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import { useAuth } from "@/app/contexts/AuthContext";
 import SignInForm from "./client-components/SignInForm";
+import { redirect } from "next/navigation";
 
 export default function SignInPage() {
+  const { session, signIn } = useAuth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="container mx-auto mt-[8rem]">
-      <SignInForm />
+      <SignInForm signIn={signIn} />
     </div>
   );
 }
--- app/components/shared/Header.tsx
+++ app/components/shared/Header.tsx
@@ -1,8 +1,11 @@
 "use client"
 
 import Image from "next/image";
+import { useAuth } from "@/app/contexts/AuthContext";
 
 const Header: React.FC = () => {
+  const { session, signOut } = useAuth();
+
   return (
     <header className="navbar bg-base-100 shadow-md p-4 h-[80px] border-b-2">
       <div className="container mx-auto flex justify-between items-center">
@@ -10,6 +13,11 @@ const Header: React.FC = () => {
           <Image src="/images/logo.png" alt="Logo" width={60} height={60} className="my-auto mx-2" />
           <a><h1 className="text-[1.5rem] ml-[8px]">エンジニアカフェ受付</h1></a>
         </div>
+        {session && (
+          <div className="flex item-center">
+            <button className="btn btn-primary" onClick={signOut}>ログアウト</button>
+          </div>
+        )}
       </div>
     </header>
   );
