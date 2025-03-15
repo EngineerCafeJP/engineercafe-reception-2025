e8b02b41b8e44d7f2c2e65971c213907f0a56035
kykt35 <kykt35@gmail.com>
2025-03-16 00:47:48 +0900

    Add SeatAreaMap components

---
+++ app/app/home/client-components/AreaBox.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Seat, SeatUsage } from '@/app/types';
+
+import React from 'react';
+import SeatCard from './SeatCard';
+
+export interface AreaBoxProps {
+  areaName: string;
+  seats: Seat[];
+  maxCol: number;
+  seatUsages: SeatUsage[];
+  onSeatClick: (seat: Seat, seatUsage: SeatUsage | null) => void;
+}
+
+export const AreaBox: React.FC<AreaBoxProps> = ({ areaName, maxCol,  seats, seatUsages, onSeatClick }) => {
+
+  return (
+    <div className="w-fit my-[1rem]">
+      <div className="text-[1rem] font-bold">{areaName}</div>
+
+      <div className={`grid gap-[0.5rem]`} style={{ gridTemplateColumns: `repeat(${maxCol}, 1fr)` }}>
+      {seats.map((seat, index) => (
+          <SeatCard key={index} seat={seat} seatUsage={seatUsages.find(usage => usage.seatId === seat.id) || null} onSeatClick={onSeatClick} />
+        ))}
+      </div>
+    </div>
+  );
+};
