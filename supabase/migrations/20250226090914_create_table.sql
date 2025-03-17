f5d2bfda3f5edd095b334ee506dec275f7d888dd
EngineerCafe A <>
2025-03-17 16:19:42 +0900

    separate mock sql

--- supabase/migrations/20250226090914_create_table.sql
+++ supabase/migrations/20250226090914_create_table.sql
@@ -142,7 +142,9 @@ CREATE TABLE
 INSERT INTO
   founds (name)
 VALUES
-  ('SNS'),
+  ('LinkedIn'),
+  ('WhatsApp'),
+  ('その他SNS（X,Facebook,etc）'),
   ('職場'),
   ('イベント'),
   ('市政だより'),
@@ -261,69 +263,6 @@ CREATE TABLE
     FOREIGN KEY (belongs_id) REFERENCES belongs (id),
     FOREIGN KEY (job_id) REFERENCES jobs (id),
     FOREIGN KEY (found_id) REFERENCES founds (id)
-  );
-
-INSERT INTO
-  users (
-    number,
-    name,
-    pronunciation,
-    email,
-    phone,
-    prefecture_id,
-    city,
-    address,
-    building,
-    belongs_id,
-    belongs_detail,
-    job_id,
-    found_id,
-    comments
-  )
-VALUES
-  (
-    '999999',
-    '山田太郎',
-    'やまだたろう',
-    'taro@example.com',
-    '09012345678',
-    (
-      SELECT
-        id
-      FROM
-        prefectures
-      WHERE
-        name = '東京都'
-    ),
-    '渋谷区',
-    '神南1',
-    '',
-    (
-      SELECT
-        id
-      FROM
-        belongs
-      WHERE
-        name = '個人'
-    ),
-    'サンプルの詳細情報です。',
-    (
-      SELECT
-        id
-      FROM
-        jobs
-      WHERE
-        name = '学生'
-    ),
-    (
-      SELECT
-        id
-      FROM
-        founds
-      WHERE
-        name = 'SNS'
-    ),
-    'サンプルのコメントです。'
   );
 
 -- NFC
