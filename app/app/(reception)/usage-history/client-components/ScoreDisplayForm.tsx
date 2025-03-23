26e021a2a7614276f3cdb8b214d0931927f8fcb3
RyotoKuroki <kurohoge@gmail.com>
2025-03-23 19:46:32 +0900

    fix: mod scorearea size

--- app/app/(reception)/usage-history/client-components/ScoreDisplayForm.tsx
+++ app/app/(reception)/usage-history/client-components/ScoreDisplayForm.tsx
@@ -13,7 +13,7 @@ const ScoreDisplayForm: React.FC<Props> = ({
   socoreOfUsers,
 }) => {
   return (
-    <div className="border-neutral-content mx-auto mt-[1.5em] max-h-[400px] max-w-[430px] border-1 py-[30px]">
+    <div className="border-neutral-content mx-auto mt-[1.5em] max-h-[400px] max-w-[430px] border-1 py-[15px]">
       <div className="columns-2 text-center">
         <div className="w-full">本日の利用数：{socoreOfUsages}</div>
         <div className="w-full">本日の利用者数：{socoreOfUsers}</div>
