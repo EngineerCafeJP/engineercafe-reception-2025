64f5cd04614a231e656278acbd63c8d01b21ca0b
RyotoKuroki <kurohoge@gmail.com>
2025-03-23 23:34:29 +0900

    fix: mod design

--- app/app/(reception)/usage-history/client-components/HistoryListViewItemForm.tsx
+++ app/app/(reception)/usage-history/client-components/HistoryListViewItemForm.tsx
@@ -23,19 +23,19 @@ const HistoryListViewItemForm: React.FC<Props> = ({
 }) => {
   return (
     <div className="flex items-center justify-between border-b border-gray-200">
-      <div className="w-2/15 p-2">
+      <div className="w-1/14 p-2">
         <div className="h-full w-full pl-[3] text-left text-[0.75em] text-gray-500">
           {rowNo}.
         </div>
       </div>
-      <div className="w-2/15 p-2">
+      <div className="w-2/14 p-2">
         <div className="flex flex-col">
           {/* // TODO: (KUROKI) use ~/components/icons/ClockIcon.tsx after pull feature-7*/}
           <div className="h-[1.8em] pt-[0.4em]">{checkInTimeStr} -</div>
           <div className="h-[1.8em] pt-[0.4em]">{checkOutTimeStr}</div>
         </div>
       </div>
-      <div className="w-7/15 p-2">
+      <div className="w-7/14 p-2">
         <div className="flex flex-col">
           <div className="h-[1.8em] pt-[0.2em]">
             {/* // TODO: (KUROKI) use ~/components/icons/SeatIcon.tsx after pull feature-7 */}
@@ -47,12 +47,12 @@ const HistoryListViewItemForm: React.FC<Props> = ({
           </div>
         </div>
       </div>
-      <div className="w-2/15 p-2">
+      <div className="w-2/14 p-2">
         <div className="h-full text-center">
           <div>{status}</div>
         </div>
       </div>
-      <div className="w-2/15 p-2">
+      <div className="w-2/14 p-2">
         <div className="h-full text-center">
           {/* // TODO: (KUROKI) add ~/components/icons/trashCanIcon.tsx */}
           <button className="rounded-full bg-blue-200 px-2 py-2 text-[0.75em] font-bold text-white hover:bg-red-200">
