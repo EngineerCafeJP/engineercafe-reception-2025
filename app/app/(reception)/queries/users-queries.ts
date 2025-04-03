d3887931177709523c8a84f821992ea6ddaa5d4e
kykt35 <kykt35@gmail.com>
2025-04-03 16:58:45 +0900

    feat: add use-search-users hook and query

---
+++ app/app/(reception)/queries/users-queries.ts
@@ -0,0 +1,26 @@
+import { SearchUserParams } from "@/app/types/search_users_params";
+import client from "@/utils/supabase/client";
+
+export const fetchUsersBySearchParams = (params?: SearchUserParams) => {
+  if (
+    params == null ||
+    (!params.id && !params.name && !params.email && !params.phone)
+  ) {
+    return { data: [], error: null };
+  }
+
+  const query = client
+    .from("users")
+    .select("*")
+    .is("is_delete", null)
+    .limit(10);
+
+  // idはint型でlike検索できないので、完全一致
+  if (params.id) {
+    return query.eq("id", params.id);
+  } else {
+    return query.or(
+      `name.like.%${params.name}%,email.like.%${params.email}%,phone.like.%${params.phone}%`,
+    );
+  }
+};
---
+++ app/app/types/search_users_params.d.ts
@@ -0,0 +1,6 @@
+export type SearchUserParams = {
+  id?: number;
+  name?: string;
+  email?: string;
+  phone?: string;
+};
