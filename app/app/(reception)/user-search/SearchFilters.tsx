bcc7b2bf2f308f7138c5072e1c3845017ff3a379
akira utsuki <a.utsuki@gmail.com>
2025-04-22 22:13:21 +0900

    feat(app): mod SearchFilters

--- app/app/(reception)/user-search/SearchFilters.tsx
+++ app/app/(reception)/user-search/SearchFilters.tsx
@@ -11,18 +11,37 @@ type Props = {
   register: UseFormRegister<Filters>;
 };
 
+const filterItems = [
+  { name: "id", text: "会員番号" },
+  { name: "email", text: "Email" },
+  { name: "phone", text: "電話番号" },
+];
+
+const FilterLabel: React.FC<{
+  filterItem: { name: string; text: string };
+  register: UseFormRegister<Filters>;
+}> = ({ filterItem, register }) => {
+  return (
+    <label className="flex items-center">
+      <input
+        className="mr-2"
+        type="checkbox"
+        {...register(filterItem.name as keyof Filters)}
+      />
+      {filterItem.text}
+    </label>
+  );
+};
+
 export default function SearchFilters({ register }: Props) {
   return (
     <div className="mb-4 flex items-center gap-4">
-      {["id", "email", "phone"].map((key) => (
-        <label key={key} className="flex items-center">
-          <input
-            className="mr-2"
-            type="checkbox"
-            {...register(key as keyof Filters)}
-          />
-          {key === "id" ? "会員番号" : key === "email" ? "Email" : "電話番号"}
-        </label>
+      {filterItems.map((filterItem) => (
+        <FilterLabel
+          key={filterItem.name}
+          filterItem={filterItem as { name: keyof Filters; text: string }}
+          register={register}
+        />
       ))}
     </div>
   );
