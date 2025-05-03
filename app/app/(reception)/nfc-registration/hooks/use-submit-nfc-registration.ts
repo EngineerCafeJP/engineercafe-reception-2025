e2aed1d5df515629f2a4ff5f47488cddfdd2e201
ryoku4 <122875619+ryoku4@users.noreply.github.com>
2025-05-03 18:12:15 +0900

    fix(app): supabase-cache-helpersのアップデート
    supabase-cache-helpersの不具合が修正されたため。
    
    fix #49

--- app/app/(reception)/nfc-registration/hooks/use-submit-nfc-registration.ts
+++ app/app/(reception)/nfc-registration/hooks/use-submit-nfc-registration.ts
@@ -6,22 +6,13 @@ export function useSubmitNfcRegistration(options?: {
   onSuccess?: (id: number) => void;
 }) {
   const { isPending, isError, isSuccess, error, mutateAsync } =
-    useInsertMutation(
-      // Use `any` type until the following issue is fixed.
-      // https://github.com/psteinroe/supabase-cache-helpers/issues/557
-      // TODO: Need to be fixed after the issue is resolved.
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      supabase.from("nfcs") as any,
-      ["id"],
-      "id",
-      {
-        onSuccess: (data) => {
-          if (data && options?.onSuccess) {
-            options.onSuccess(data[0].id);
-          }
-        },
+    useInsertMutation(supabase.from("nfcs"), ["id"], "id", {
+      onSuccess: (data) => {
+        if (data && options?.onSuccess) {
+          options.onSuccess(data[0].id);
+        }
       },
-    );
+    });
 
   return {
     isPending,
@@ -29,11 +20,7 @@ export function useSubmitNfcRegistration(options?: {
     isSuccess,
     error,
     insert: async (data: NfcRegistrationSchema) => {
-      // Use `any` type until the following issue is fixed.
-      // https://github.com/psteinroe/supabase-cache-helpers/issues/557
-      // TODO: Need to be fixed after the issue is resolved.
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      await mutateAsync({ user_id: data.userId, nfc_id: data.cardId } as any);
+      await mutateAsync([{ user_id: data.userId, nfc_id: data.cardId }]);
     },
   };
 }
